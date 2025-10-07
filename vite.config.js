d0b7606eac50d7aed64bb60bc4e1f12f903d5c36
0paziz <itzabdiaziz@gmail.com>
2025-10-07 18:31:20 +0300

    removed the local server proxy configuration

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss(),],
-   server: {
-    port: 5173, // your Vite dev port
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000", // your backend server
-        changeOrigin: true,
-        secure: false,}
-      }
-    }
 })
